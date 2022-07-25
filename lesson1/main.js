e23b4df4a5fa9bbc0da0d31b2a4f7519c2aa95e6
JuliaKolosovska <ykolosovska@gmail.com>
2022-07-25 10:03:54 +0200

    add files

---
+++ lesson1/main.js
@@ -0,0 +1,62 @@
+let a = 'hello';
+let b = 'owu';
+let c = 'com';
+let d = 'ua';
+let e = -1;
+let f = 10;
+let g = -999;
+let h = 123;
+let j = 3.14;
+let k = 2.7;
+let l = 16
+let m = true;
+let n = false;
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(g);
+console.log(h);
+console.log(j);
+console.log(k);
+console.log(l);
+console.log(m);
+console.log(n);
+alert(a);
+alert(b);
+alert(c);
+alert(d);
+alert(e);
+alert(f);
+alert(g);
+alert(h);
+alert(j);
+alert(k);
+alert(l);
+alert(m);
+alert(n);
+document.write(`${a} ${b}.${c}.${d} ${e} ${f} ${g} ${h} ${j} ${k} ${l} ${m} ${n}`);
+
+let firstName = 'Юлія';
+let middleName = 'Сергіївна';
+let lastName = 'Колосовська';
+let person = lastName + ' ' + firstName + ' ' + middleName;
+console.log(person);
+let a1=100;
+let b1='100';
+let c1=true;
+console.log(typeof a1);
+console.log(typeof b1);
+console.log(typeof c1);
+let name=prompt('Введіть ім`я');
+console.log(name)
+let fatherName=prompt('Введіть по-батькові');
+console.log(fatherName)
+let age=prompt('Ваш вік');
+console.log(age)
+
+
+
+
