d37278cd4f7a81bdc52259c8a4c115b7709b5f3c
JuliaKolosovska <ykolosovska@gmail.com>
2022-08-10 17:17:08 +0200

    add files

--- lesson6/main.js
+++ lesson6/main.js
@@ -119,61 +119,72 @@
 //     color:'', // 'red','black'
 // }
 
-let cards=[
-    {cardSuit:'spade' , value:'6', color:'black'},
-    {cardSuit:'spade' , value:'7', color:'black'},
-    {cardSuit:'spade' , value:'8', color:'black'},
-    {cardSuit:'spade' , value:'9', color:'black'},
-    {cardSuit:'spade' , value:'9', color:'black'},
-    {cardSuit:'spade' , value:'10', color:'black'},
-    {cardSuit:'spade' , value:'ace', color:'black'},
-    {cardSuit:'spade' , value:'jack', color:'black'},
-    {cardSuit:'spade' , value:'queen', color:'black'},
-    {cardSuit:'spade' , value:'king', color:'black'},
-    {cardSuit:'spade' , value:'joker', color:'black'},
-    {cardSuit:'clubs' , value:'6', color:'black'},
-    {cardSuit:'clubs' , value:'7', color:'black'},
-    {cardSuit:'clubs' , value:'8', color:'black'},
-    {cardSuit:'clubs' , value:'9', color:'black'},
-    {cardSuit:'clubs' , value:'9', color:'black'},
-    {cardSuit:'clubs' , value:'10', color:'black'},
-    {cardSuit:'clubs' , value:'ace', color:'black'},
-    {cardSuit:'clubs' , value:'jack', color:'black'},
-    {cardSuit:'clubs' , value:'queen', color:'black'},
-    {cardSuit:'clubs' , value:'king', color:'black'},
-    {cardSuit:'clubs' , value:'joker', color:'black'},
-    {cardSuit:'diamond' , value:'6', color:'red'},
-    {cardSuit:'diamond' , value:'7', color:'red'},
-    {cardSuit:'diamond' , value:'8', color:'red'},
-    {cardSuit:'diamond' , value:'9', color:'red'},
-    {cardSuit:'diamond' , value:'9', color:'red'},
-    {cardSuit:'diamond' , value:'10', color:'red'},
-    {cardSuit:'diamond' , value:'ace', color:'red'},
-    {cardSuit:'diamond' , value:'jack', color:'red'},
-    {cardSuit:'diamond' , value:'queen', color:'red'},
-    {cardSuit:'diamond' , value:'king', color:'red'},
-    {cardSuit:'diamond' , value:'joker', color:'red'},
-    {cardSuit:'heart' , value:'6', color:'red'},
-    {cardSuit:'heart' , value:'7', color:'red'},
-    {cardSuit:'heart' , value:'8', color:'red'},
-    {cardSuit:'heart' , value:'9', color:'red'},
-    {cardSuit:'heart' , value:'9', color:'red'},
-    {cardSuit:'heart' , value:'10', color:'red'},
-    {cardSuit:'heart' , value:'ace', color:'red'},
-    {cardSuit:'heart' , value:'jack', color:'red'},
-    {cardSuit:'heart' , value:'queen', color:'red'},
-    {cardSuit:'heart' , value:'king', color:'red'},
-    {cardSuit:'heart' , value:'joker', color:'red'}];
+// let cards=[
+//     {cardSuit:'spade' , value:'6', color:'black'},
+//     {cardSuit:'spade' , value:'7', color:'black'},
+//     {cardSuit:'spade' , value:'8', color:'black'},
+//     {cardSuit:'spade' , value:'9', color:'black'},
+//     {cardSuit:'spade' , value:'9', color:'black'},
+//     {cardSuit:'spade' , value:'10', color:'black'},
+//     {cardSuit:'spade' , value:'ace', color:'black'},
+//     {cardSuit:'spade' , value:'jack', color:'black'},
+//     {cardSuit:'spade' , value:'queen', color:'black'},
+//     {cardSuit:'spade' , value:'king', color:'black'},
+//     {cardSuit:'spade' , value:'joker', color:'black'},
+//     {cardSuit:'clubs' , value:'6', color:'black'},
+//     {cardSuit:'clubs' , value:'7', color:'black'},
+//     {cardSuit:'clubs' , value:'8', color:'black'},
+//     {cardSuit:'clubs' , value:'9', color:'black'},
+//     {cardSuit:'clubs' , value:'9', color:'black'},
+//     {cardSuit:'clubs' , value:'10', color:'black'},
+//     {cardSuit:'clubs' , value:'ace', color:'black'},
+//     {cardSuit:'clubs' , value:'jack', color:'black'},
+//     {cardSuit:'clubs' , value:'queen', color:'black'},
+//     {cardSuit:'clubs' , value:'king', color:'black'},
+//     {cardSuit:'clubs' , value:'joker', color:'black'},
+//     {cardSuit:'diamond' , value:'6', color:'red'},
+//     {cardSuit:'diamond' , value:'7', color:'red'},
+//     {cardSuit:'diamond' , value:'8', color:'red'},
+//     {cardSuit:'diamond' , value:'9', color:'red'},
+//     {cardSuit:'diamond' , value:'9', color:'red'},
+//     {cardSuit:'diamond' , value:'10', color:'red'},
+//     {cardSuit:'diamond' , value:'ace', color:'red'},
+//     {cardSuit:'diamond' , value:'jack', color:'red'},
+//     {cardSuit:'diamond' , value:'queen', color:'red'},
+//     {cardSuit:'diamond' , value:'king', color:'red'},
+//     {cardSuit:'diamond' , value:'joker', color:'red'},
+//     {cardSuit:'heart' , value:'6', color:'red'},
+//     {cardSuit:'heart' , value:'7', color:'red'},
+//     {cardSuit:'heart' , value:'8', color:'red'},
+//     {cardSuit:'heart' , value:'9', color:'red'},
+//     {cardSuit:'heart' , value:'9', color:'red'},
+//     {cardSuit:'heart' , value:'10', color:'red'},
+//     {cardSuit:'heart' , value:'ace', color:'red'},
+//     {cardSuit:'heart' , value:'jack', color:'red'},
+//     {cardSuit:'heart' , value:'queen', color:'red'},
+//     {cardSuit:'heart' , value:'king', color:'red'},
+//     {cardSuit:'heart' , value:'joker', color:'red'}];
 //
 //
 
-
+// let spadeAce = cards.find((card) => {
+//         if (card.cardSuit === 'spade' && card.value === 'ace') {
+//             console.log(card)
+//     }
+// }
+// );
 // let allSix=cards.filter(value =>value.value==='6');
 // console.log(allSix);
 // let allRed=cards.filter(value => value.color==='red');
 // console.log(allRed);
 // let allDiamond=cards.filter(value => value.cardSuit==='diamond');
 // console.log(allDiamond);
+// let allClubs=cards.filter(value => value.cardSuit==='clubs');
+// console.log(allClubs.subarray(3));
+
+ //останнє чомусь показує помилку, ніби такої функції не існує. В інтернеті пише, що таке є,
+// я робила по принципу ".substring", який Сергій показував нам на лекції
+
 
 
 //
